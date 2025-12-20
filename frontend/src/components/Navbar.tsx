b1446cff50ecf992c53977e9c23c43de9340529d
miralriodev <miralriodev@gmail.com>
2025-12-19 19:11:10 -0600

    feat(ui):cleaning ui for dashboard with logo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Navigate } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="py-4 px-4 md:px-16 bg-zinc-950 shadow-sm mb-4 flex items-center justify-between">
-          <h1 className="font-semibold text-base md:text-lg text-zinc-50 truncate">Cotizador de locales <span className="text-xl md:text-2xl">🏪</span></h1>
+    <nav className="py-4 px-4 md:px-36 bg-zinc-950 shadow-sm mb-4 flex items-center justify-between">
+          <h1 className="flex gap-2 font-semibold text-base md:text-lg text-zinc-50 truncate"><img className="w-auto h-4 md:h-7 bg-auto" src="/logo.png" alt="" /><p className=" hidden md:block">Cotizador de locales</p> </h1>
           <span onClick={() => Navigate({to: "/"})} className="cursor-pointer text-base md:text-lg font-semibold text-zinc-200 hover:text-white transition-colors">Dashboard</span>
         </nav>
   )
